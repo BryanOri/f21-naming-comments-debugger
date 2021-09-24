0a267265e53ea75b09fdeb10a96d9984a3ab4ac2
BryanOri <bryanorihuela@computingforall.org>
2021-09-23 18:07:32 -0800

    stuff

--- naming-comments-debugger.js
+++ naming-comments-debugger.js
@@ -3,26 +3,24 @@
 
 /* Uncomment the below instruction for the
      debugger to pause execution of all JavaScript */
-// debugger;
+     debugger;
 
-/* (1) Rename these identifiers to label what the function
-         is doing and what the variables are holding
-   (2) Add a comment above the function describing what it does
-
-   Note: This is the Pythagorean theorem */
-function z(y,x)
-{
-	let w = (y * y) + (x * x);
-	let v = Math.sqrt(w);
-	return v;
-}
-
-/* (3) Add Single Line Comments after each of the three
-     instructions below to tell us what each "w" value
-     is inside the method being called */
-console.log(z(3, 4));
-console.log(z(7, 21));
-console.log(z(17, 14));
-
-// This is an example of what the above lines should look like
-console.log(z(10, 5)); // w = 125
+     /* (1) Rename these identifiers to notify what the function
+            is doing and what the variables are holding
+        (2) Add a comment above the function describing what it does */
+        //it does the calculations of whatever number is being imputted within
+        function z(y,x)
+        {
+            let Multiply = (y * y) + (x * x);
+            let SquareRoot = Math.sqrt(Multiply);
+            return SquareRoot;
+        }
+        console.log(z(10, 5)); // w = 125
+        
+        /* (3) Add Single Line Comments after each of the three
+             instructions below to tell us what each "w" value
+             is inside the method being called */
+        console.log(z(3, 4));//11.180339887498949
+        console.log(z(7, 21));//5
+        console.log(z(17, 14));//22.135943621178654
+        
